d612a42af7b1d2200c948b0073d8e16b9efa4bb8
NooranTarek <noort.mohamed@gmail.com>
2024-05-06 09:27:40 +0300

    adding enum column (role) for user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->enum('role', ['admin', 'candidate', 'employer'])->nullable();
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
