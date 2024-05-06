630f0d7e2522c56ba9ef0d9a0e54f101ac27f716
NooranTarek <noort.mohamed@gmail.com>
2024-05-06 10:21:55 +0300

    delete all users

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//to delete all users if we create seeder and then we want to delete all 
 Route::delete('/users/deleteAll', [UserController::class, 'deleteAllUsers']);
 Route::apiResource('users', UserController::class);
