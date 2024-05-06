432f1ff75e6f0f6058a8eff824209c19bfc4759d
Ahmed Khaled <ahmedkhaled4219@gmail.com>
2024-05-06 21:30:19 +0300

    Create CRUD operations for job

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ApplicationController; // Corrected namespace
+use App\Http\Controllers\Api\JobListingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/applications', [ApplicationController::class, 'store']);
+
+Route::apiResource('/jobs', JobListingController::class);
