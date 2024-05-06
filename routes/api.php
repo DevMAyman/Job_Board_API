e04ebd7ecd792b9187a7a6900489554082e4ed50
NooranTarek <noort.mohamed@gmail.com>
2024-05-06 09:56:02 +0300

    adding initial api for user

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('users', UserController::class);
