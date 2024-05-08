61d130722280db0753369c5f1dff127e494ffdcd
NooranTarek <noort.mohamed@gmail.com>
2024-05-08 14:12:31 +0300

    modifying authentication with returning token as json

--- routes/api.php
+++ routes/api.php
@@ -7,47 +7,39 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cookie; // Add this line
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
- 
 
 Route::post('/sanctum/token', function (Request $request) {
     if (Auth::attempt($request->only('email', 'password'))) {
         $user = Auth::user();
 
-        // Customize the token payload
         $userData = [
-            'id' => $user->id,
             'name' => $user->name,
             'role' => $user->role,
-            // Add any other user data you want to include
         ];
 
-        // Create a token with Sanctum and attach it to the user
         $token = $user->createToken($request->device_name, ['userData' => $userData]);
-
-        // Set the token as an HTTP-only cookie
-        $cookie = cookie('token', $token->plainTextToken, 60); // Set cookie for 60 minutes
 
         return response()->json([
             'message' => 'Authenticated',
             'token' => [
                 'plainTextToken' => $token->plainTextToken,
-                'user' => $userData // Include user data in the response
+                'user' => $userData 
             ]
-        ])->withCookie($cookie);
+        ]);
     }
 
     return response()->json(['error' => 'Unauthorized'], 401);
 });
 
+// Custom middleware for role-based authorization
+// Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
 
-
-Route::middleware('auth:sanctum')->group(function () {
-    // To delete all users if we create a seeder and then we want to delete all 
+Route::middleware(['auth:sanctum', 'role'])->group(function () {
     Route::delete('/users/deleteAll', [UserController::class, 'deleteAllUsers']);
     Route::apiResource('users', UserController::class);
 });
+
