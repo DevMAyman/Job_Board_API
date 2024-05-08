24129810985fe2ba34bb496982c03206fdb5d2ae
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-05-08 14:36:39 +0300

    laravel auth

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,11 +14,10 @@ return [
     | and production domains which access your API via a frontend SPA.
     |
     */
-
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        'localhost:8080'
     ))),
 
     /*
