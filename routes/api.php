6c29cd1ab17afb9e3aef22cc65f7aaacd510108b
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-05-11 22:57:40 +0300

    solve conflicts

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,22 @@
 <?php
 
-use App\Http\Controllers\API\ApplicationController;
-use App\Http\Controllers\API\JobListingController;
-use App\Http\Controllers\API\LogoutController;
-use App\Http\Controllers\API\RegisterController;
-use App\Http\Controllers\API\UserController;
-// use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth; // Corrected namespace
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\LogoutController;
+use App\Http\Controllers\API\ApplicationController; // Corrected namespace
+use App\Http\Controllers\API\JobListingController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 Route::post('/login', [UserController::class, 'login']);
 Route::delete('/users/deleteAll', function (Request $request) {
-    if ($request->user()->role === 'employer') {
-        return (new UserController())->deleteAllUsers($request);
-    } else {
-        return response()->json(['error' => 'Unauthorized'], 401);
-    }
-})->middleware('auth:sanctum');
     if ($request->user()->role === 'employer') {
         return (new UserController())->deleteAllUsers($request);
     } else {
@@ -39,6 +34,7 @@ Route::controller(RegisterController::class)->group(function () {
 });
 Route::post('logout', LogoutController::class)->middleware('auth:sanctum');
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -46,9 +42,3 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('/applications', ApplicationController::class)->middleware('auth:sanctum');
 
 Route::apiResource('/jobs', JobListingController::class)->middleware('auth:sanctum');
-
-
-
-Route::get('/usersPaginate', function (Request $request) {
-    return User::search($request->input('query'))->paginate(15);
-});
