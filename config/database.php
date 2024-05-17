cc0a73b056f420525164b29765b54b468c5f3ddb
DevMAyman <eng.developer.mohamed.ayman@gmail.com>
2024-05-17 19:33:27 +0300

    database connection updated

--- config/database.php
+++ config/database.php
@@ -16,8 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-    
+    'default' => env('DB_CONNECTION', 'postgres'),
 
     /*
     |--------------------------------------------------------------------------
